528f8d6c2bc560a41ea78809c81c9af57c1ba957
CCDM22 <ccdm22@CCDM22.local>
2018-04-26 12:49:10 -0600

    Marvel con series e historias

--- marvel/js/index.js
+++ marvel/js/index.js
@@ -4,6 +4,11 @@ var marvel = function(){
 		var personaje = $("#txtPersonaje").val();
 		var url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="
 		var cantidadComics = 0;
+		var comics = "";
+		var cantidadSeries = 0;
+		var series = "";
+		var cantidadHistorias = 0;
+		var historias = "";
 		url = url + personaje;
 		$.ajax({
 			dataType:"json",
@@ -12,11 +17,39 @@ var marvel = function(){
 				if (response.code == 200) {
 					$("#foto").attr("src",response.data.results[0].thumbnail.path
 					+"."+response.data.results[0].thumbnail.extension);
+					$("#nombre").html(response.data.results[0].name);
+					cantidadComics=response.data.results[0].comics.returned;
+					cantidadSeries=response.data.results[0].series.returned;
+					cantidadHistorias=response.data.results[0].stories.returned;
+
+
+					for (var i = 0; i < cantidadComics; i++) {
+						comics+=response.data.results[0].comics.items[i].name+"<br>"
+					}
+					$("#comics").html("comics: <br>"+comics);
+
+					for (var i = 0; i < cantidadSeries; i++) {
+						series+=response.data.results[0].series.items[i].name+"<br>"
+					}
+					$("#series").html("series: <br>"+series);
+
+					for (var i = 0; i < cantidadHistorias; i++) {
+						historias+=response.data.results[0].stories.items[i].name+"<br>"
+					}
+					$("#historias").html("Historias: <br>"+historias);
 				}
 
 				}
 		});
 	}
+	var teclaPersonaje = function(tecla){
+		//Enter = 10 + 13
+		//donde 10=Retorno de Carro y 13=Avance de Linea
+		if (tecla.which == 13) {//Enter
+			Buscar();
+		}
+	}
 	$("#btnBuscar").on("click",Buscar);
+	$("#txtPersonaje").on("keypress",teclaPersonaje)
 }
 $(document).ready(marvel);
